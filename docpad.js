6f9e71cab99e38916b266ffa8820c80e3d1adc5e
Alan Raphael <alan@xr1.com.br>
2015-11-06 16:38:09 -0200

    Dev.

--- docpad.js
+++ docpad.js
@@ -7,7 +7,7 @@ module.exports = {
 
     // Conference info
     conf: {
-		name: "OpenLabs",
+		name: "OpenLabs Teste",
 		description: "OpenLabs - 1º Conferência #OpenDev",
 		date: "05 de Dezembro",
 		// If your event is free, just comment this line
